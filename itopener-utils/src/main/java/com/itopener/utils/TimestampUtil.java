fecb61f168009f235b7b1502249654cbe18507aa
vip96300@163.com <Hhf135797531>
2019-06-17 09:43:58 +0800

    commit

---
+++ itopener-utils/src/main/java/com/itopener/utils/TimestampUtil.java
@@ -0,0 +1,15 @@
+package com.itopener.utils;
+
+import java.sql.Timestamp;
+
+/**  
+ * @author fuwei.deng
+ * @Date 2017年6月9日 下午3:10:58
+ * @version 1.0.0
+ */
+public class TimestampUtil {
+
+	public static final Timestamp current(){
+		return new Timestamp(System.currentTimeMillis());
+	}
+}
